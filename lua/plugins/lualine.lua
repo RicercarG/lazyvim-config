575b075485fbfafec749b18be60027dc41fef7ac
RicercarG <104607918+RicercarG@users.noreply.github.com>
2025-09-16 22:18:57 -0400

    lualine show filetype for inactive files

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -25,7 +25,8 @@ return {
     },
 
     inactive_sections = {
-      lualine_a = {
+      lualine_a = { "filetype" },
+      lualine_b = {
         { "filename", path = 1 },
       },
       lualine_c = {},
